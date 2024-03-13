ca3f34a2e0a0f53b131d95c3166dbcd3ea3693f3
Julian <jjvll90@gmail.com>
2024-03-12 20:45:41 -0500

    Refactor Gallery component to use react-photo-album

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,25 +1,5 @@
-function Gallery() {
-// Placeholder data for your images
-const images = [
-{ src: 'path/to/image1.jpg', title: 'Image 1', description: 'Description of image 1' },
-{ src: 'path/to/image2.jpg', title: 'Image 2', description: 'Description of image 2' },
-// Add more images here
-];
+import PhotoAlbum from "react-photo-album";
+import photos from "./photos";
 
-return (
-<div>
-    <h1>My Photography Gallery</h1>
-    <div className="gallery">
-    {images.map((image, index) => (
-        <div key={index} className="photo-card">
-        <img src={image.src} alt={image.title} />
-        <h2>{image.title}</h2>
-        <p>{image.description}</p>
-        </div>
-    ))}
-    </div>
-</div>
-);
-}
 
-export default Gallery;
+<PhotoAlbum layout="masonry" photos={photos} />
